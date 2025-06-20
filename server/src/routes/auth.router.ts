330f4ad26b90bf7ee36bab8bc97f9da712a285ca
Martin Metodiev <martin.metodiew@gmail.com>
2025-06-20 20:43:24 +0300

    feat(server): implement input validation rules for user registration endpoint

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 
 import * as authController from './auth/auth.controller';
+import { handleInputErrors } from '../middleware/validation.middleware';
 
 const authRouter = Router();
 
-authRouter.post('/register', authController.register);
+authRouter.post(
+  '/register',
+  body('handle').notEmpty().withMessage('The Handle cannot be empty'),
+  body('name').notEmpty().withMessage('The Name cannot be empty'),
+  body('email').isEmail().withMessage('Invalid email address'),
+  body('password')
+    .isLength({ min: 8 })
+    .withMessage('The password must be at least 8 characters long'),
+  handleInputErrors,
+  authController.register
+);
 
 export default authRouter;
